72461788aa52d10447d7e6df05f3501a013162cd
Contempt00 <hitup.jmarks@gmail.com>
2018-05-30 20:34:06 -0400

    Added redirection for install page
    Installed and used file-system (fs)
    Added redirection to install.html when appropriate.
    Added install.html
    Tested on MacOS

--- website/web.js
+++ website/web.js
@@ -6,13 +6,19 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var filesystem = require('fs');
 
 // Serve our (static) public directory so CSS/IMGs work.
 app.use(express.static(__dirname + '/public/'));
 
 app.get('/', function(req, res){
     // Serve our views directory (for index.html/homepage)
-    res.sendFile(__dirname + '/views/');
+    if(!filesystem.existsSync(__dirname+'/../discord/settings.json')){
+        // Settings file doesn't exist send user to install page
+        res.sendFile(__dirname + '/views/install.html');
+    }else {
+        res.sendFile(__dirname + '/views/');
+    }
 });
 
 io.on('connection', function(socket){
@@ -24,3 +30,6 @@ process.on('message', function(m){
 });
 
 server.listen(80, function listenCallback() {console.log("Control Panel Starting on port 80");});
+server.on('error', function httpServerError(e) {
+    console.log(`[Web.js, Error]: ${e}`);
+});
